df29f1512b9f24043cce214cf71703fc5e3fe9b1
survival8764 <82863756+survival8764@users.noreply.github.com>
2024-05-20 11:20:48 +0900

    add : stats
    게임 테두리 collider 추가, 스텟추가,

--- Assets/Script/Player/Bullet.cs
+++ Assets/Script/Player/Bullet.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public float speed = 15.0f;
+    public float speed;
     private AttackSO AttackSO;
+    private Vector2 direction;
+    private CharacterStat characterStat;
+
     private void Update()
     {
-        this.transform.Translate(Vector3.up * this.speed * Time.deltaTime);    
+        this.transform.Translate(Vector3.up * this.speed * Time.deltaTime);
     }
 
-    public void SetSo(AttackSO So)
+    public void SetDirection(Vector2 direction)
     {
-        AttackSO = So;
+        this.direction = direction;
     }
+
+    public void SetSo(AttackSO attackSO)
+    {
+        AttackSO = attackSO;
+    }
+
 }
