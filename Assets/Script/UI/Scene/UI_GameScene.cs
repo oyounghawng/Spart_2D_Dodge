a7e7252c6889a910939a03e0dd95231add938153
PuEE1004 <167049220+PuEE1004@users.noreply.github.com>
2024-05-22 11:01:16 +0900

    폭탄 이펙트..이게맞는지?

--- Assets/Script/UI/Scene/UI_GameScene.cs
+++ Assets/Script/UI/Scene/UI_GameScene.cs
@@ -6,9 +6,11 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 public class UI_GameScene : UI_Scene
 {
+    public int boom;
     enum GameObjects
     {
         Boom,
+        boomEffect
     }
     enum Texts
     {
@@ -37,9 +39,36 @@ public class UI_GameScene : UI_Scene
         Time.timeScale = 0f;
         Managers.UI.ShowPopupUI<UI_Pause>();
     }
+    public GameObject boomEffect;
     void Boom(PointerEventData evt)
     {
         //Boom ·ÎÁ÷ÀÛ¼º!
         Debug.Log("ºÕ!");
+        if (Input.GetButton("Boom"))
+        {
+            return;
+        }
+        if(boom == 0)
+        {
+            return;
+        }
+        boom -= 1;
+
+        boomEffect.SetActive(true);
+
+        Invoke("offBoomEffect", 4f);
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        for (int i = 0; i < enemies.Length; i++) 
+        {
+            Enemy enemyScript = enemies[1].GetComponent<Enemy>();
+            enemyScript.OnHit(1000);
+        }
+        GameObject[] enemyBullet = GameObject.FindGameObjectsWithTag("EnemyBullet");
+        for(int i = 0; i <enemyBullet.Length; i++)
+        {
+            Destroy(enemyBullet[i]);
+        }
+
+        
     }
 }
